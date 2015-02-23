8e5e496b53b68dd97fcb35a592b0b2960fa707bc
Hiroyuki Sano <sh19910711@gmail.com>
2015-02-23 17:04:46 +0900

    app: add social_snippet()

--- lib/completer/app.rb
+++ lib/completer/app.rb
@@ -1,6 +1,10 @@
 module Completer
 
-  class Application < ::Padrino::Application; end
+  class Application < ::Padrino::Application
+    def social_snippet
+      @social_snippet ||= ::SocialSnippet::Core.new
+    end
+  end
 
 end
 

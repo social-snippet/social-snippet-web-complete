1442f1e0ac99e270ce91fbcf4f21143bb29721cb
Hiroyuki Sano <sh19910711@gmail.com>
2015-02-23 17:22:57 +0900

    app_controller: call complete api

--- lib/completer/controllers/app_controller.rb
+++ lib/completer/controllers/app_controller.rb
@@ -9,9 +9,18 @@
   }
 
   get :index, :provides => :json do
-    {
-      :status => "OK"
-    }.to_json
+    begin
+      res = social_snippet.api.complete_snippet_path(params[:q])
+      {
+        :status => "OK",
+        :result => res,
+      }.to_json
+    rescue => e
+      {
+        :status => "ERROR",
+        :error => e.to_s,
+      }.to_json
+    end
   end
 
 end

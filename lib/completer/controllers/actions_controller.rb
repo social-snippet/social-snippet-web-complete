a333981148a25049f25f8dd367b58010b7f89869
Hiroyuki Sano <sh19910711@gmail.com>
2015-02-23 17:15:21 +0900

    actions_controller: add POST /actions/install

--- lib/completer/controllers/actions_controller.rb
+++ lib/completer/controllers/actions_controller.rb
@@ -13,6 +13,12 @@
   end
 
   post :install, :provides => :json do
+    params[:repos].each do |repo_name|
+      social_snippet.api.install_repository_by_name repo_name, nil
+    end
+    {
+      :status => "OK",
+    }.to_json
   end
 
 end

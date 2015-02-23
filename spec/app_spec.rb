7e8329884e8ac4c21885a689f2dd5c7b461adf01
Hiroyuki Sano <sh19910711@gmail.com>
2015-02-23 17:47:35 +0900

    test: add tests for actions

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -7,32 +7,59 @@ describe ::Completer::Application do
   end
 
   context "GET /" do
+
     before { get "/" }
     subject { last_response }
     it { should be_ok }
+
     context "parse json" do
       subject { ::JSON.parse last_response.body }
       it { should have_key "status" }
       it { expect(subject["status"]).to eq "OK" }
     end
+
   end
 
   describe "actions" do
 
-    context "POST /repos" do
-      let(:params) do
+    context "POST /actions/install" do
+
+      let(:install_params) do
         {
           :repos => ["example-repo"],
         }
       end
+
       let(:headers) do
         {
           "Content-Type" => "application/json",
         }
       end
-      before { post "/actions/install", params, headers }
+
+      before { post "/actions/install", install_params, headers }
       subject { last_response }
       it { should be_ok }
+
+      context "POST /actions/complete" do
+
+        let(:complete_params) do
+          {
+            :q => "// @snip <ex",
+          }
+        end
+
+        before { post "/actions/complete", complete_params, headers }
+        subject { last_response }
+        it { should be_ok }
+
+        context "parse json" do
+          subject { ::JSON.parse last_response.body }
+          it { should have_key "result" }
+          it { expect(subject["result"]).to include "example-repo" }
+        end
+
+      end
+
     end
 
   end # actions

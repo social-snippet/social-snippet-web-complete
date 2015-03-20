92373698df81a0e55ffc673e480312b7c6fd601f
Hiroyuki Sano <sh19910711@gmail.com>
2015-03-20 13:57:47 +0900

    config/boot.rb: apply default configuration to Completer::Application

--- config/boot.rb
+++ config/boot.rb
@@ -1,17 +1,14 @@
 RACK_ENV = ENV["RACK_ENV"] ||= "development" unless defined?(RACK_ENV)
 PADRINO_ROOT = ::File.expand_path("../..", __FILE__) unless defined?(PADRINO_ROOT)
 
-require "padrino-core"
-require "mongoid"
-
-::Padrino.configure_apps do
-end
+require "bundler/setup"
+Bundler.require :default, RACK_ENV
 
 ::Padrino.before_load do
-  require "mongoid"
 end
 
 ::Padrino.after_load do
+  ::Completer::Application.default_configuration!
 end
 
 ::Padrino.load!

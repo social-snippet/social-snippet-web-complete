282ed14a26f249d9b3e4d6f393e5b1aff2be2034
Hiroyuki Sano <sh19910711@gmail.com>
2015-03-20 13:57:33 +0900

    config/apps.rb: setup rack-protection

--- config/apps.rb
+++ config/apps.rb
@@ -1,1 +1,11 @@
+::Padrino.configure_apps do
+  logger.info "Setup rack-protection"
+  disable :sessions
+  set :protection, false
+  set :protect_from_csrf, false
+  use ::Rack::Session::Pool, :key => "s", :path => "/", :expire_after => nil
+  use ::Rack::Protection
+  use ::Rack::Protection::AuthenticityToken
+end
+
 ::Padrino.mount("Completer::Application").to("/")

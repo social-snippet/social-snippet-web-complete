8beb1acb38ed4d40ac52ee5a24796f8a88923dcd
Hiroyuki Sano <sh19910711@gmail.com>
2015-03-20 15:26:45 +0900

    app_controller: add GET /token

--- lib/completer/controllers/app_controller.rb
+++ lib/completer/controllers/app_controller.rb
@@ -7,6 +7,17 @@
     }.to_json
   end
 
+  get :token, :provides => :json do
+    {
+      :token => session["csrf"],
+    }.to_json
+  end
+
+  get :repos, :provides => :json do
+    {
+    }.to_json
+  end
+
   configure :development do
 
     get :secret do

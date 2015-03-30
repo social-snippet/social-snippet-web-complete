46b5bf638e2e3682e340d645470c4568d249faed
Hiroyuki Sano <sh19910711@gmail.com>
2015-03-30 11:01:12 +0900

    completer: add helpers.rb

--- lib/completer/controllers/actions_controller.rb
+++ lib/completer/controllers/actions_controller.rb
@@ -27,39 +27,6 @@
     }.to_json
   end
 
-  helpers do
-    def new_ranges(src, dest)
-      ranges = []
-      lines = []
-      cnt = 0
-      last_cnt = -1
-
-      # find new lines
-      ::Diff::LCS.sdiff(src, dest).each do |change|
-        if change.action == "+" && cnt > last_cnt
-          last_cnt = cnt
-          lines.push cnt
-        end
-        if change.new_element == "\n"
-          cnt += 1
-        end
-      end
-
-      # convert to range
-      from = nil
-      actual = lines.first
-      lines.to_a.slice_before do |cur|
-        expected, actual = actual.next, cur
-        expected != actual
-      end.map do |seq|
-        {
-          :front => seq.first,
-          :to => seq.last,
-        }
-      end
-    end
-  end
-
   # Insert snippets
   post :insert, :provides => :json do
     res = social_snippet.api.insert_snippet(params[:text])
